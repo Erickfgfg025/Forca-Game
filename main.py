26d62ce856e19ea23dd335b926d096cd30902329
Erick Sergio <ericksergio138@gmail.com>
2025-05-26 14:08:26 -0300

    Atualizar o main.py
    adicionamos uma tela que é chamada quando clicar no botão iniciar jogo
    criamos o botao chutar,a entrada e os textos de acertos

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Jogo:
        
         self.tela.geometry("800x600")
         self.tela.title("Jogo da Forca")
-        self.tela['bg']='black'
+        self.tela['bg']="white"
         self.tela.resizable(False,False)
         
         
@@ -18,31 +18,52 @@ class Jogo:
         self.fundo = image.open("/imagens/telainicial-fundo.png")
         self.fundo = self.fundo.resize((700,500))
         self.fundo_tk = ImageTk.PhotoImage(self.fundo)
-
+        
 '''
+        self.palavra = "Python"
+        self.descoberta = ["_"] * len(self.palavra)
+        self.palavras_errada = []
+        self.chances = 6
+
         self.fundo = PhotoImage(file="imagens/telainicial-fundo.png")
         
         
         #construo todos os botões e textos 
         self.imagem = Label(self.tela,image = self.fundo)
-        self.titulo = Label(self.tela, text="Jogo da Forca", fg="black",font=('Tahoma',14,'bold'))
+        self.titulo = Label(self.tela, text="Jogo da Forca", fg="black",font=('Tahoma',12,'bold'))
         
-        self.btn_iniciar = Button(self.tela,text="INICIAR JOGO",fg='grey')
+        self.btn_iniciar = Button(self.tela,text="INICIAR JOGO",fg='black',command=self.iniciarjogo,relief="flat")
+        
+        
         
         
         #desenho todos botões e textos
         self.imagem.place(x=0,y=0,relwidth=1,relheight=1)
         self.titulo.pack(pady=(80,10))
-        self.btn_iniciar.pack(pady=(350,10))
+        self.btn_iniciar.pack(pady=(350,20))
 
-        self.tela.mainloop()
+        self.tela.mainloop() # faz o código rodar
         
     def iniciarjogo(self):
-        pass
-        
-        
+      self.apaga_tudo() #apaga tudo da tela 
+      
+      self.palavra = Label(self.tela,text=" ".join(self.descoberta),font=("Arial",14,"bold"),bg="white")
+      self.chute = Entry(self.tela,width=10)
+      self.lbl_tentativas = Label(self.tela, text= f"Tentativas: {self.chances}",font=("Arial",6))
+      self.btn_chutar = Button(self.tela, text="Chutar",font=("Arial",6),relief="flat")
+      
+      
+      
+      #manda desenha na tela 
+      self.palavra.place(x=400-100,y=300)
+      self.chute.place(x=300,y=480)
+      self.lbl_tentativas.place(x=10, y=370)
+      self.btn_chutar.place(x=480,y=470)
+   
     def apaga_tudo(self):
-        pass
+        self.titulo. destroy()
+        self.btn_iniciar.destroy()
+        self.imagem.place_forget()
         
         
         
