708fa0f12ca3d387d0600898e8405c3e39c36dd4
Erick Sergio <ericksergio138@gmail.com>
2025-05-30 13:20:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-#from PIL import image, imageTk
+from PIL import Image, ImageTk
 
 
 class Jogo:
@@ -11,33 +11,40 @@ class Jogo:
         self.tela.title("Jogo da Forca")
         self.tela['bg']="white"
         self.tela.resizable(False,False)
+
+
+        self.frame1 = Frame(self.tela)
+        #self.frame2 = Frame(self.tela)
+        self.frame1.pack(fill="both",expand=True)
+        #self.frame2.pack(fill="both")
         
         
+        self.cor_texto = "#E0D9C8"
         #carrega todas as imagens 
-        '''
-        self.fundo = image.open("/imagens/telainicial-fundo.png")
-        self.fundo = self.fundo.resize((700,500))
+       
+        self.fundo = Image.open("imagens/telainicial-fundo.png")
+        self.fundo = self.fundo.resize((800,600))
         self.fundo_tk = ImageTk.PhotoImage(self.fundo)
         
-'''
-        self.palavra = "Python"
+
+        self.palavra = "PYTHON"
         self.descoberta = ["_"] * len(self.palavra)
         self.palavras_errada = []
         self.chances = 6
 
-        self.fundo = PhotoImage(file="imagens/telainicial-fundo.png")
         
         
         #construo todos os botões e textos 
-        self.imagem = Label(self.tela,image = self.fundo)
-        self.titulo = Label(self.tela, text="Jogo da Forca", fg="black",font=('Tahoma',12,'bold'))
         
-        self.btn_iniciar = Button(self.tela,text="INICIAR JOGO",fg='black',command=self.iniciarjogo,relief="flat")
+        self.imagem = Label(self.frame1,image = self.fundo_tk)
+        self.titulo = Label(self.frame1, text="Jogo da Forca", fg="black",font=('Verdana',25,'bold'))
+        self.btn_iniciar = Button(self.frame1,text="Iniciar Jogo",font=("Verdana",16),fg="black", bd=5, bg=self.cor_texto,command=self.iniciarjogo,relief="flat")
         
         
         
         
         #desenho todos botões e textos
+        
         self.imagem.place(x=0,y=0,relwidth=1,relheight=1)
         self.titulo.pack(pady=(80,10))
         self.btn_iniciar.pack(pady=(350,20))
@@ -45,26 +52,42 @@ class Jogo:
         self.tela.mainloop() # faz o código rodar
         
     def iniciarjogo(self):
-      self.apaga_tudo() #apaga tudo da tela 
-      
-      self.palavra = Label(self.tela,text=" ".join(self.descoberta),font=("Arial",14,"bold"),bg="white")
-      self.chute = Entry(self.tela,width=10)
-      self.lbl_tentativas = Label(self.tela, text= f"Tentativas: {self.chances}",font=("Arial",6))
-      self.btn_chutar = Button(self.tela, text="Chutar",font=("Arial",6),relief="flat")
+        self.frame1.pack_forget()
+        
+        self.imagem2 = Label(self.tela,image=self.fundo_tk)
+        self.lbl_palavra = Label(self.tela,text=" ".join(self.descoberta),font=("Arial",30,"bold"),bg="white",bd=1)
+        self.chute = Entry(self.tela,width=10,bd=5)
+        self.lbl_tentativas = Label(self.tela, text= f"Tentativas: {self.chances}",font=("Arial",12))
+
+        self.btn_chutar = Button(self.tela, text="Chutar",command=self.tentar,font=("Verdana",12,"bold"),relief="flat",bg="grey",fg="white")
       
       
       
       #manda desenha na tela 
-      self.palavra.place(x=400-100,y=300)
-      self.chute.place(x=300,y=480)
-      self.lbl_tentativas.place(x=10, y=370)
-      self.btn_chutar.place(x=480,y=470)
-   
-    def apaga_tudo(self):
-        self.titulo. destroy()
-        self.btn_iniciar.destroy()
-        self.imagem.place_forget()
+        self.imagem2.place(x=0,y=0,relwidth=1,relheight=1)
+        self.lbl_palavra.place(x=420-100,y=300)
+        self.chute.place(x=410,y=470)
+        self.lbl_tentativas.place(x=10, y=370)
+        self.btn_chutar.place(x=480,y=470)
+
+
+    def tentar(self):
         
+        self.letra = self.chute.get().upper()
+        self.chute.delete(0,END)
+        if self.letra in self.palavra:
+            for i,l in enumerate(self.palavra):
+                if l==self.letra:
+                    self.descoberta[i]= self.letra
+
+        else:
+            self.chances -= 1
+
+        self.update()
+
+    def update(self):
+        self.lbl_palavra["text"]=" ".join(self.descoberta)
+        self.lbl_tentativas["text"] = f"Tentativas restantes: {self.chances}"
         
         
 app = Jogo()
